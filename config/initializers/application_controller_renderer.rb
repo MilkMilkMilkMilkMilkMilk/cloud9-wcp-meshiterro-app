fcc8b1b5c08c402dff3fb51f3b46df6fafc530eb
MilkMilkMilkMilkMilkMilk <izumilk0922@gmail.com>
2021-12-15 12:28:08 +0000

    [Finish] 4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'e8f6c10a6403967d02a479ab74d0832467af7e8bdcf5cdfa64c16176bdedca82874036195ce46680030713839386f97e2ac29f02464ffe62ef01ed9b84cfe06a'
